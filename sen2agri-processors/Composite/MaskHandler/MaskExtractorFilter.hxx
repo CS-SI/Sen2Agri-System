dd41dff168c6218ee184c1555363b14ce2f6874a
Mickaël Savinaud <mickael.savinaud@c-s.fr>
2017-11-29 00:51:21 +0100

    [STYLE] remove unnecessary comments and files

--- sen2agri-processors/Composite/MaskHandler/MaskExtractorFilter.hxx
+++ sen2agri-processors/Composite/MaskHandler/MaskExtractorFilter.hxx
@@ -15,19 +15,9 @@
  
 #include "MaskExtractorFilter.h"
 
-//  Software Guide : EndCodeSnippet
-
-//  Software Guide : BeginLatex
-//
-//  The constructor sets up the pipeline, which involves creating the
-//  stages, connecting them together, and setting default parameters.
-//
-//  Software Guide : EndLatex
-
 namespace otb
 {
 
-//  Software Guide : BeginCodeSnippet
 template <class TImageType, class TOutputImageType>
 MaskExtractorFilter<TImageType, TOutputImageType>
 ::MaskExtractorFilter()
@@ -37,22 +27,6 @@ MaskExtractorFilter<TImageType, TOutputImageType>
     this->SetNumberOfRequiredInputs(2);
 }
 
-//  Software Guide : EndCodeSnippet
-
-//  Software Guide : BeginLatex
-//
-//  The \code{GenerateData()} is where the composite magic happens.  First,
-//  we connect the first component filter to the inputs of the composite
-//  filter (the actual input, supplied by the upstream stage).  Then we
-//  graft the output of the last stage onto the output of the composite,
-//  which ensures the filter regions are updated.  We force the composite
-//  pipeline to be processed by calling \code{Update()} on the final stage,
-//  then graft the output back onto the output of the enclosing filter, so
-//  it has the result available to the downstream filter.
-//
-//  Software Guide : EndLatex
-
-//  Software Guide : BeginCodeSnippet
 template <class TImageType, class TOutputImageType>
 void
 MaskExtractorFilter<TImageType, TOutputImageType>::
@@ -77,8 +51,6 @@ SetBitsMask(int cloudMask, int waterMask, int snowMask)
     m_MaskHandlerFunctor->GetFunctor().SetBitsMask(cloudMask, waterMask, snowMask);
 }
 
-//  Software Guide : EndCodeSnippet
-
 template <class TImageType, class TOutputImageType>
 void
 MaskExtractorFilter<TImageType, TOutputImageType>::
@@ -89,15 +61,6 @@ UpdateOutputInformation()
     this->GetOutput()->SetNumberOfComponentsPerPixel(3);
 }
 
-//  Software Guide : BeginLatex
-//
-//  Finally we define the \code{PrintSelf} method, which (by convention)
-//  prints the filter parameters.  Note how it invokes the superclass to
-//  print itself first, and also how the indentation prefixes each line.
-//
-//  Software Guide : EndLatex
-//
-//  Software Guide : BeginCodeSnippet
 template <class TImageType, class TOutputImageType>
 void
 MaskExtractorFilter<TImageType, TOutputImageType>::
